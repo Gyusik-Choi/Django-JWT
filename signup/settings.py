0443d5a5fa215423dfdfe6a062acf072a0f815d1
gyusik <kuidoli@hanmail.net>
2020-12-23 00:38:49 +0900

    modify secrets.json

--- signup/settings.py
+++ signup/settings.py
@@ -15,12 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -137,3 +133,6 @@ def get_secret(setting, secrets=secrets):
 
 SECRET_KEY_JWT = get_secret("SECRET_KEY_JWT")
 ALGORITHM = get_secret("ALGORITHM")
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = get_secret("SECRET_KEY")

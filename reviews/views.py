787c8c75262b598a2d650765a7757d77cb1464b8
gyusik <kuidoli@hanmail.net>
2020-12-30 23:03:07 +0900

    modify reviews

--- reviews/views.py
+++ reviews/views.py
@@ -4,11 +4,14 @@ from .models import Review
 from .serializers import ReviewSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from signup.utils import login_decorator
 
 @api_view(['POST'])
+@login_decorator
 def create_review(request):
+    print(1)
     serializer = ReviewSerializer(data=request.data)
     print(request.data)
     if serializer.is_valid(raise_exception=True):
-        serializer.save(user=request.user)     
+        serializer.save(user=request.user)
     return Response(serializer.data)
---
+++ signup/utils.py
@@ -0,0 +1,21 @@
+import jwt                                                
+import json
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import JsonResponse
+from .settings import SECRET_KEY_JWT, ALGORITHM
+
+def login_decorator(func):
+    def wrapper(request, *args, **kwargs):
+        try:
+            access_token = request.headers.get('Authorization')
+            payload = jwt.decode(access_token, SECRET_KEY_JWT, algorithm=ALGORITHM)
+            User = get_user_model()
+            user = User.objects.get(email=payload['email'])
+            request.user = user
+            return func(request, *args, **kwargs)
+
+        except jwt.exceptions.DecodeError:
+            return JsonResponse({'message' : 'INVALID_TOKEN' }, status=400)
+
+    return wrapper
